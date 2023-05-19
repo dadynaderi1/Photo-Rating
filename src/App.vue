d3eda69072bc6d59f39765e1a8c73355378466cd
Dave <dadynaderi1@gmail.com>
2023-05-19 06:00:59 +0330

    refactoring the file browser

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FileBrowser from './components/FileBrowser.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+  name:'App',
+  components:{FileBrowser}
 }
+
 </script>
 
+<template>
+  <div class="wrapper">
+    <FileBrowser/>
+ 
+</div>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700";
+body {
+    font-family: 'Poppins', sans-serif;
+    background: #fafafa;
 }
+.wrapper {
+    display: flex;
+    width: 100%;
+    align-items: stretch;
+}
+
 </style>
---
+++ src/components/FileBrowser.vue
@@ -0,0 +1,104 @@
+<script>
+import fs from 'fs'
+import pathModule from 'path'
+
+import { app } from '@electron/remote'
+import { computed, ref } from 'vue'
+
+import FilesViewer from './FilesViewer'
+
+const formatSize = size => {
+  var i = Math.floor(Math.log(size) / Math.log(1024))
+  return (
+    (size / Math.pow(1024, i)).toFixed(2) * 1 +
+    ' ' +
+    ['B', 'kB', 'MB', 'GB', 'TB'][i]
+  )
+}
+
+export default {
+  name: 'FileBrowser',
+  components: { FilesViewer },
+  setup() {
+    const path = ref(app.getAppPath())
+    const files = computed(() => {
+      const fileNames = fs.readdirSync(path.value)
+      return fileNames
+        .map(file => {
+          const stats = fs.statSync(pathModule.join(path.value, file))
+          return {
+            name: file,
+            size: stats.isFile() ? formatSize(stats.size ?? 0) : null,
+            directory: stats.isDirectory()
+          }
+        })
+        .sort((a, b) => {
+          if (a.directory === b.directory) {
+            return a.name.localeCompare(b.name)
+          }
+          return a.directory ? -1 : 1
+        })
+    })
+
+    const back = () => {
+      path.value = pathModule.dirname(path.value)
+    }
+    const open = folder => {
+      path.value = pathModule.join(path.value, folder)
+    }
+
+    const searchString = ref('')
+    const filteredFiles = computed(() => {
+      return searchString.value
+        ? files.value.filter(s => s.name.startsWith(searchString.value))
+        : files.value
+    })
+
+    return {
+      path,
+
+      open,
+      back,
+
+      files,
+      searchString,
+      filteredFiles
+    }
+  }
+}
+
+</script>
+
+<template>
+  <!-- Sidebar -->
+  <nav id="sidebar">
+      <div class="sidebar-header">
+          <h5>{{ path }}</h5>
+      </div>
+
+      <ul class="list-unstyled components">
+            <FilesViewer
+      :files="filteredFiles"
+      :nested="nested"
+      @back="back"
+      @folderclick="open($event.name)"
+    />
+      </ul>
+  </nav>
+</template>
+
+<style scoped>
+@import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700";
+ul ul a {
+    font-size: 0.9em !important;
+    padding-left: 30px !important;
+}
+#sidebar {
+  min-width: 300px;
+max-width: 300px;
+min-height: 100vh;
+background: #1d1919;
+color: #fff;
+}
+
+</style>
